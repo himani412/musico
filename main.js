811546db3018eba93f9fe59e843e07427793703c
himani412 <77782996+himani412@users.noreply.github.com>
2021-07-30 03:01:56 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
+
 song="";
+leftwristx=0;
+leftwristy=0;
+rightwristx=0;
+rightwristy=0;
+
 function preload(){
     song=loadSound("music.mp3");
 }
@@ -7,6 +13,11 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotposes);
+}
+function modelLoaded(){
+    console.log('ModelLoaded');
 }
 function draw(){
     image(video,0,0,600,500);
@@ -14,4 +25,17 @@ function draw(){
 }
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+function gotposes(results){
+    if(results.length > 0){
+        console.log(results);
+        leftwristx=results[0].pose.leftWrist.x;
+        leftwristy=results[0].pose.leftWrist.y;
+        console.log("LeftWristX = " + leftwristx + " LeftWristY = " + leftwristy);
+        rightwristx=results[0].pose.rightWrist.x;
+        rightwristy=results[0].pose.rightWrist.y;
+        console.log("RightWristX = " + rightwristx + " RightWristY = " + rightwristy);
+    }
 }
