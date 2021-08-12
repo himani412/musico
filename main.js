afd5b5f661a8bcd77bc976261f8f8e78e5f76f0b
himani412 <77782996+himani412@users.noreply.github.com>
2021-08-12 02:06:14 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 scoreleftwrist=0;
+scorerightwrist=0;
 songplay="";
 song1="";
 song2="";
@@ -25,11 +26,10 @@ function modelLoaded(){
 function draw(){
     image(video,0,0,600,500);
      song1="music.mp3";
-     song2="song2.mp3";
      fill("#0000FF");
     stroke("#0000FF");
     if(scoreleftwrist > 0.2){
-    circle(leftwristx,rightwristx,20);
+    circle(leftwristx,leftwristy,20);
 
     song.song2.stop()
     if(song1 == false){
@@ -37,22 +37,34 @@ function draw(){
         document.getElementById("songname").innerHTML= "Song Played 1";
 }
 }
+if(scorerightwrist > 0.2){
+    circle(rightwristx,rightwristy,20);
+
+    song.song1.stop()
+    if(song2 == false){
+        song.play(song2);
+        document.getElementById("songname").innerHTML= "Song Played 2";
+}
+}
+
 }
 function play(){
     song.play();
     song.setVolume(1);
     song.rate(1);
 }
-function gotposes(results){
-    if(results.length > 0){
-        console.log(results);
-        scoreleftwrist=results[0].pose.keypoints[9].score;
-        console.log("scoreleftwrist = "+ scoreleftwrist);
-        leftwristx=results[0].pose.leftWrist.x;
-        leftwristy=results[0].pose.leftWrist.y;
-        console.log("LeftWristX = " + leftwristx + " LeftWristY = " + leftwristy);
-        rightwristx=results[0].pose.rightWrist.x;
-        rightwristy=results[0].pose.rightWrist.y;
-        console.log("RightWristX = " + rightwristx + " RightWristY = " + rightwristy);
+    function gotposes(results){
+        if(results.length > 0){
+            console.log(results);
+            scorerightwrist=results[0].pose.keypoints[10].score;
+            console.log("scorerightwrist = "+ scorerightwrist);
+            scoreleftwrist=results[0].pose.keypoints[9].score;
+            console.log("scoreleftwrist = "+ scoreleftwrist);
+            leftwristx=results[0].pose.leftWrist.x;
+            leftwristy=results[0].pose.leftWrist.y;
+            console.log("LeftWristX = " + leftwristx + " LeftWristY = " + leftwristy);
+            rightwristx=results[0].pose.rightWrist.x;
+            rightwristy=results[0].pose.rightWrist.y;
+            console.log("RightWristX = " + rightwristx + " RightWristY = " + rightwristy);
+        }
     }
-}
